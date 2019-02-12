5cf7dd849820b65a736f9aadcb85313c5a2821db
lilian waweru <lilowesh.lw@gmail.com>
2019-02-12 11:59:38 +0300

    js operators

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = add(number1, number2);
+//alert(result);
+function subtract(number1, number2){
+  return number1 - number2;
+};
+//alert(subtract(number1, number2));
+
+function multiply(number1, number2){
+  return number1 * number2;
+};
+//alert(multiply(number1, number2));
+
+function division(number1, number2){
+  return number1 / number2;
+};
+alert(division(number1, number2));
